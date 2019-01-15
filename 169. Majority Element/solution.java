928c4c592cb69b6c88c7c4989cadf79678c142c7
hotfics <hotfics@foxmail.com>
2019-01-15 10:17:42 +0800

    easy

---
+++ 169. Majority Element/solution.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int[] nums={3,2,3};
+        int m= majorityElement(nums);
+        System.out.println(m);
+
+    }
+    public static int majorityElement(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for (int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        int[] count=new int[set.size()];
+        for(int s:set){
+            long n= Arrays.stream(nums).filter(x->x==s).count();
+            if (n>nums.length/2) return s;
+        }
+
+        return 0;
+
+    }
+
+
+}
+
+
+
+
+
